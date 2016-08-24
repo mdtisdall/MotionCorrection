82fda7f3ac0581774cc3e83cd5c7a99bdf48969a
Dylan Tisdall <tisdall@nmr.mgh.harvard.edu>
2016-08-24 10:28:44 -0400

    Now using mix-in for residuals in new Gauss_Newton class

---
+++ ResidualOpBase.h
@@ -0,0 +1,29 @@
+#ifndef ResidualOpBase_h
+#define ResidualOpBase_h
+
+template < typename T, typename CoordT >
+class ResidualOpBase {
+public:
+  typedef Eigen::Matrix< T, 3, 1 > PointT;
+  typedef Eigen::Matrix<T, Eigen::Dynamic, 1> ResidualT;
+  typedef typename Eigen::Matrix<T, 6, 1> ParamT;
+  typedef Eigen::Map< Eigen::Matrix<T, Eigen::Dynamic, 1 > > NewVolVecT;
+  typedef Eigen::Matrix< T, 3, Eigen::Dynamic > PointListT;
+
+protected:
+  ResidualOpBase(const size_t cubeSize) :
+    cubeSize(cubeSize),
+    cubeCenter(cubeCenterFromCubeSize(cubeSize)),
+    cubeCenterPoint(cubeCenter, cubeCenter, cubeCenter) {}
+
+  static CoordT cubeCenterFromCubeSize(const size_t cubeSize) {
+      return ((CoordT) cubeSize)/(CoordT)2.0 - (CoordT)0.5;
+  }
+
+  const size_t cubeSize;
+  const CoordT cubeCenter;
+  const PointT cubeCenterPoint;
+};
+
+#endif
+
